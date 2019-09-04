a2cab9c7e355908f361f2fca7b6ee98526ae762c
Emil Alexandrescu <emil.alexandresku@gmail.com>
2019-09-04 03:23:20 -0700

    basic layout components

---
+++ src/containers/layout/Content.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Box } from 'rebass';
+
+const Content = props => {
+  return <Box py={3} {...props} />;
+};
+
+export default Content;
---
+++ src/containers/layout/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Box } from 'rebass';
+
+const Header = props => {
+  return <Box {...props}>Rembrance Logo</Box>;
+};
+
+export default Header;
