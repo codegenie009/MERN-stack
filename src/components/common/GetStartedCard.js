7d819d8d6d0813e7c85732ee6ae69752e2523879
Emil Alexandrescu <emil.alexandresku@gmail.com>
2019-09-09 10:57:19 -0700

    get started card

---
+++ src/components/common/GetStartedCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Flex, Text } from 'rebass';
+
+function GetStartedCard({ title, icon, children }) {
+  return (
+    <Flex
+      variant="card"
+      width={['calc(25% - 30px)']}
+      textAlign="center"
+      flexDirection="column"
+      alignItems="center"
+      pt={50}
+      px={20}
+      height={300}
+    >
+      <Text as="i" fontSize={40} className={`far fa-${icon}`} mb={25} />
+      <Text variant="h4" mb={15}>
+        {title}
+      </Text>
+      <Text variant="body3" color="text3">
+        {children}
+      </Text>
+    </Flex>
+  );
+}
+
+GetStartedCard.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.string,
+  icon: PropTypes.string
+};
+
+export default GetStartedCard;
