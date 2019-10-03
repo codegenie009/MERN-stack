2f4b2dd03320c0f7bb493a01b3b1da12e043bd6b
Emil Alexandrescu <emil.alexandresku@gmail.com>
2019-10-03 09:53:03 -0700

    image file upload field

---
+++ src/components/formik/FileUploadField.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Uploader } from 'components/form';
+import Field from './Field';
+
+const FileUploadField = ({
+  field,
+  form,
+  label,
+  sublabel,
+  rightIcon,
+  helpText,
+  labelProps,
+  ...otherProps
+}) => {
+  const handleChange = v => {
+    form.setFieldValue(field.name, v);
+  };
+
+  return (
+    <Field
+      form={form}
+      name={field.name}
+      label={label}
+      sublabel={sublabel}
+      helpText={helpText}
+      labelProps={labelProps}
+      render={() => (
+        <Uploader onChange={handleChange} value={field.value} {...otherProps} />
+      )}
+    />
+  );
+};
+
+FileUploadField.propTypes = {
+  field: PropTypes.object,
+  form: PropTypes.object,
+  rightIcon: PropTypes.node,
+  label: PropTypes.string,
+  sublabel: PropTypes.string,
+  helpText: PropTypes.string,
+  labelProps: PropTypes.object,
+  submitCount: PropTypes.number
+};
+
+export default FileUploadField;
