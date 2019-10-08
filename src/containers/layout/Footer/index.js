89d8fc37e28e44628d3caad24bf6dcfb6aedeac1
Emil Alexandrescu <emil.alexandresku@gmail.com>
2019-10-08 13:42:40 -0700

    hide footer

--- src/containers/layout/Footer/index.js
+++ src/containers/layout/Footer/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { MainSelectors } from 'redux/MainRedux';
 // import HomeFooter from './HomeFooter';
 import SimpleFooter from './SimpleFooter';
 
 const NO_FOOTER_LINKS = ['/space-create'];
 
 // @TODO home footer?
-function Footer({ location }) {
+function Footer({ location, isLoggedIn }) {
   if (NO_FOOTER_LINKS.includes(location.pathname)) {
+    return null;
+  }
+
+  if (isLoggedIn) {
     return null;
   }
 
@@ -16,7 +23,17 @@ function Footer({ location }) {
 }
 
 Footer.propTypes = {
-  location: PropTypes.object
+  location: PropTypes.object,
+  isLoggedIn: PropTypes.bool
 };
 
-export default withRouter(Footer);
+const mapStatesToProps = state => ({
+  isLoggedIn: MainSelectors.selectLoggedIn(state)
+});
+
+const enhance = compose(
+  withRouter,
+  connect(mapStatesToProps)
+);
+
+export default enhance(Footer);
